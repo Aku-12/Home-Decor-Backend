920ea2f4989f056b8c81858183b1c80ed9af32ff
Aku-12 <akashchaudhary205698@gmail.com>
2024-07-28 00:19:47 +0545

    orderitem, order and payment pojo added in pojo package for the entity and repo

---
+++ HomeDecor/src/main/java/com/example/homedecor/Pojo/OrderItemPojo.java
@@ -0,0 +1,12 @@
+package com.example.homedecor.Pojo;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderItemPojo {
+    private Long furnitureId;
+    private int quantity;
+}
---
+++ HomeDecor/src/main/java/com/example/homedecor/Pojo/PaymentPojo.java
@@ -0,0 +1,27 @@
+package com.example.homedecor.Pojo;
+
+import com.example.homedecor.Entity.Orders;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+
+public class PaymentPojo {
+    private Integer id;
+
+    private String type;
+
+    private String amount;
+    private LocalDate date;
+    private Orders order;
+}
+
